26d52ecb606e50415a48a8076ff4309ef7a5781b
Choo Kyunghyun <chookyunghyun@naver.com>
2024-09-16 03:20:04 +0900

    Update playlists page layout and functionality

--- scripts/playlists.js
+++ scripts/playlists.js
@@ -1,178 +1,110 @@
 "use strict";
 
-// Get elements
-const playlists = document.getElementById("playlists");
-const search = document.getElementById("search");
-const artists = document.getElementById("artists");
-
-// Artists set
-let artists_set = new Set();
-
-// Add playlist card
-function playlist_add(_track, _artists, _album, _release_year, _duration_string, _thumbnail, _url, _tags) {
-    // Create track
-    let track = document.createElement("p");
-    track.style.fontWeight = "bold";
-    track.textContent = _track;
-    // Create artists
-    let artists = document.createElement("p");
-    artists.textContent = _artists.join(", ");
-    // Create album
-    let album = document.createElement("p");
-    if (_release_year === "None") {
-        album.textContent = _album;
-    } else {
-        album.textContent = _album + " (" + _release_year + ")";
+// Track class
+class Track {
+    constructor(_track, _artists, _album, _year, _duration, _thumbnail, _url, _tags) {
+        this.track = _track;
+        this.artists = _artists;
+        this.album = _album;
+        this.year = _year;
+        this.duration = _duration;
+        this.thumbnail = _thumbnail;
+        this.url = _url;
+        this.tags = _tags;
     }
-    // Create duration
-    let duration = document.createElement("p");
-    duration.textContent = _duration_string;
-    // Create thumbnail
-    let thumbnail = document.createElement("img");
-    thumbnail.classList.add("logo");
-    thumbnail.src = _thumbnail;
-    thumbnail.alt = _track;
-    // Create icon
-    let icon = document.createElement("img");
-    icon.classList.add("icon");
-    icon.src = "/images/youtube-music-icon.svg";
-    icon.alt = "YouTube Music";
-    // Create url
-    let url = document.createElement("a");
-    url.href = _url;
-    url.target = "_blank";
-    // Add icon to link
-    url.appendChild(icon);
-    // Create tags
-    let tags = document.createElement("p");
-    tags.textContent = _tags.join(", ");
-    tags.style.display = "none";
-    // Create card
-    let card = document.createElement("div");
-    card.classList.add("card");
-    // Add elements to card
-    card.appendChild(thumbnail);
-    card.appendChild(track);
-    if (_album === "None") {
-        album.remove();
-    } else {
-        card.appendChild(album);
+
+    card() {
+        let card = document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML = `
+            <img class="logo" src="${this.thumbnail}" alt="${this.track}">
+            <p style="font-weight: bold;">${this.track}</p>
+            ${this.album !== "None" ? `<p>${this.year == "None" ? this.album : this.album + " (" + this.year + ")"}</p>` : ""}
+            <p>${this.artists.join(", ")}</p>
+            <p>${this.duration}</p>
+            <a href="${this.url}" target="_blank"><img class="icon" src="/images/youtube-music-icon.svg" alt="YouTube Music"></a>
+            <p style="display: none;">${this.tags.join(", ")}</p>
+        `;
+        return card;
     }
-    card.appendChild(artists);
-    card.appendChild(duration);
-    card.appendChild(url);
-    card.appendChild(tags);
-    // Add card to playlists
-    playlists.appendChild(card);
 }
 
-// Parse CSV
-function parse_csv_line(line) {
-    const regex = /(?:\"([^\"]*(?:\"\"[^\"]*)*)\")|([^,]+)/g;
-    let matches = [];
-    let match;
-    while ((match = regex.exec(line)) !== null) {
-        matches.push(match[1] ? match[1].replace(/\"\"/g, '"') : match[2]);
+// Playlist class
+class Playlist {
+    constructor(_url, _showcase) {
+        this.tracks = [];
+        this.artists = new Map();
+        this.showcase = _showcase;
+        this.parse(_url);
     }
-    return matches;
-}
 
-// Parse CSV list
-function convert_list(field) {
-    // Check if field is empty
-    if (field === "None") {
-        return [];
-    }
-    // Remove [ and ] from field
-    field = field.substring(1, field.length - 1);
-    // Use regex to split by comma not inside quotes and trim each item
-    let items = field.match(/('(?:\\.|[^'\\])*'|[^,]+)/g).map(item => item.trim().replace(/^'/, "").replace(/'$/, ""));
-    // Return items
-    return items;
-}
-
-// Process CSV
-function process_csv(text) {
-    let lines = text.split("\n").map(line => line.trim()).filter(line => line.length > 0);
-    let items = lines.map(line => {
-        let [track, artists, album, release_year, duration_string, thumbnail, url, tags] = parse_csv_line(line);
-        artists = convert_list(artists);
-        tags = convert_list(tags);
-        return { track, artists, album, release_year, duration_string, thumbnail, url, tags };
-    });
-    return items;
-}
-
-// Add artist to artists
-function artist_add(artist) {
-    let card = document.createElement("div");
-    card.classList.add("card");
-    let text = document.createElement("p");
-    text.textContent = artist;
-    card.appendChild(text);
-    artists.appendChild(card);
-}
-
-// Load playlists from CSV
-function load_csv() {
-    // Fetch CSV
-    fetch(playlists.getAttribute("url"))
-        // Parse CSV
-        .then(response => response.text())
-        .then(text => {
-            // Parse CSV lines
-            let items = process_csv(text);
-            // Sort items
-            items.sort((a, b) => {
-                let cmp = a.artists[0].localeCompare(b.artists[0]);
-                if (cmp === 0) {
-                    return a.track.localeCompare(b.track);
-                } else {
-                    return cmp;
-                }
-            });
-            // Add artists to artists set
-            items.forEach(item => {
-                item.artists.forEach(artist => artists_set.add(artist));
-            });
-            // Add artists to artists
-            artists_set.forEach(artist => artist_add(artist));
-            // Add items to playlists
-            items.forEach(item => {
-                playlist_add(item.track, item.artists, item.album, item.release_year, item.duration_string, item.thumbnail, item.url, item.tags);
-            });
-        });
-}
-
-// Search playlists
-search.addEventListener("input", () => {
-    let query = search.value.toLowerCase();
-    let cards = playlists.getElementsByClassName("card");
-    let lists = artists.getElementsByClassName("card");
-    for (let card of cards) {
-        let keywords = card.getElementsByTagName("p");
-        let found = false;
-        for (let keyword of keywords) {
-            if (keyword.textContent.toLowerCase().includes(query)) {
-                found = true;
-                break;
+    add(track) {
+        this.tracks.push(track);
+        track.artists.forEach(artist => {
+            if (this.artists.has(artist)) {
+                this.artists.set(artist, this.artists.get(artist) + 1);
+            } else {
+                this.artists.set(artist, 1);
             }
-        }
-        if (found) {
-            card.style.display = "block";
-        } else {
-            card.style.display = "none";
-        }
+        });
     }
-    for (let list of lists) {
-        let text = list.getElementsByTagName("p")[0];
-        if (text.textContent.toLowerCase().includes(query)) {
-            list.style.display = "block";
-        } else {
-            list.style.display = "none";
-        }
+
+    sort() {
+        this.tracks.sort((a, b) => {
+            let cmp = a.artists[0].localeCompare(b.artists[0]);
+            if (cmp === 0) {
+                return a.track.localeCompare(b.track);
+            }
+            return cmp;
+        });
+        this.artists = new Map([...this.artists.entries()].sort((a, b) => b[0] - a[0]));
     }
+
+    convert_list(_string) {
+        const regex = /,\s*(?=(?:[^']*'[^']*')*[^']*$)/;
+        const quote = /^'|'$/g;
+        return _string.slice(1, -1).split(regex).map(item => item.replace(quote, "").trim());
+    }
+
+    parse(_url) {
+        const regex = /(?:"([^"]*(?:""[^"]*)*)")|([^,]+)/g;
+        this.showcase.innerHTML = "";
+        this.tracks = [];
+        this.artists = new Map();
+        fetch(_url)
+            .then(response => response.text())
+            .then(text => {
+                let lines = text.split("\n").map(line => line.trim()).filter(line => line.length > 0);
+                lines.forEach(line => {
+                    let [track, artists, album, year, duration, thumbnail, url, tags] = [...line.matchAll(regex)].map(match => match[1] ? match[1].replace(/""/g, '"') : match[2]);
+                    this.add(new Track(track, this.convert_list(artists), album, year, duration, thumbnail, url, this.convert_list(tags)));
+                });
+            })
+            .then(() => {
+                this.sort();
+                this.tracks.forEach(track => {
+                    this.showcase.appendChild(track.card());
+                });
+            })
+            .catch(error => console.error(error));
+    }
+}
+
+// Playlist instance
+const playlists = document.getElementById("playlists");
+const playlist = new Playlist(playlists.value, document.getElementById("playlist"));
+
+// Change playlist
+playlists.addEventListener("change", () => {
+    playlist.parse(playlists.value);
 });
 
-// Load playlists
-load_csv();
+// Search playlist
+document.getElementById("search").addEventListener("input", (event) => {
+    let query = event.target.value.toLowerCase();
+    let cards = playlist.showcase.getElementsByClassName("card");
+    for (let card of cards) {
+        let context = card.textContent.toLowerCase();
+        card.style.display = context.includes(query) ? "block" : "none";
+    }
+});
